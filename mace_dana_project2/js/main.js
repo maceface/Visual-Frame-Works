9dbb50d19cb613d0c46ab450302fd8266ba084cf
Dana Mace <maceface@mac.com>
2013-01-22 11:39:10 -0500

    nothing working
    but nearly everything changed

--- mace_dana_project2/js/main.js
+++ mace_dana_project2/js/main.js
@@ -20,6 +20,15 @@ window.addEventListener("DOMContentLoaded", function(){
             alert("There are no Gratitudes in your library.");
         }
         
+            //Function - erase grats in local
+    function clearLibrary(){
+        if(localStorage.length === 0){
+            alert("There are no gratitudes to delete.");
+            window.location.reload();
+            return false;
+        }
+    };
+        
         //From local to browser
         var makeConversion = document.createEl('div');
         makeConversion.setAttribute("id", "choices");
@@ -67,44 +76,43 @@ window.addEventListener("DOMContentLoaded", function(){
         }
     };
     
-    //Store Grat in Local Storage
-    function storeGrat(){
-        var gratId = Math.floor(Math.random()*100000002)
-        
-        //Store form field values in an object
-        //Objects props - array with form labels and input values
-        var choice = {};
-        choice.date = ["Date:", $('date').value];
-        choice.time = ["Time:", $('time').value];
-        choice.what = ["What I'm grateful for:", $('what').value];
-        choice.why = ["I have an attitude of gratitude because:", $('why').value];
-        choice.color = ["What color do you like best today?:", getFavValue()];
-        
-        //Get CheckBox Value
+    
+            //Get Color Value
         function getFavValue(){
             if($('fav').checked){
                 favValue = $('fav').value;
             } else {
                 favValue = "No"
             }
-            
             return favValue
         };
+    
+    //Store Grat in Local Storage
+    function storeGrat(){
+        var gratId = Math.floor(Math.random()*100000001)
         
-        //Save data to local storage: conv obj to a string
+        //Store form field values in an object
+        //Objects props - array with form labels and input values
+        var choice = {};
+            choice.date = ["Date:", $('date').value];
+            choice.time = ["Time:", $('time').value];
+            
+            choice.what = ["What I'm grateful for:", $('what').value];
+            
+            choice.why = ["I have an attitude of gratitude because:", $('why').value];
+            
+            choice.color = ["What color do you like best today?:", getFavValue()];
+            
+            choice.items = ["Which items make you happy?", itemsValue];
+    //Save data to local storage: conv obj to a string
         localStorage.setChoice(gratId, JSON.stringify(choice));
         alert("Gratitude Added!");
     };
     
-    //Function - erase grats in local
-    function clearLibrary(){
-        if(localStorage.length === 0){
-            alert("There are no gratitudes to delete.");
-            window.location.reload();
-            return false;
-        }
-    };
-    
+    //Variable defaults
+    var colorsGroups = ["yellow", "pink", "blue", "green", "red"],
+        itemsValue
+        
     //Set link and submit click events
     var displayGrat = $("displayGrat");
     displayGrat.addEventListener("click", showGrat);
@@ -112,4 +120,4 @@ window.addEventListener("DOMContentLoaded", function(){
     clearGrat.addEventListener("click", clearLibrary);
     var saveGrat = $("saveGrat");
     saveGrat.addEventListener("click", storeData);
-})
+});
