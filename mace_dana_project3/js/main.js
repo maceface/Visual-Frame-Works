ea64a9e44e62066de873dccf7394a8550aadad0e
Dana Mace <maceface@mac.com>
2013-01-22 23:04:15 -0500

    changed a lot
    but still not storing in local storage

--- mace_dana_project3/js/main.js
+++ mace_dana_project3/js/main.js
@@ -11,54 +11,41 @@ window.addEventListener("DOMContentLoaded", function(){
     function $(x){
         var theEl = document.getElementById(x);
         return theEl;
-    };
-        
-            //Function - erase grats in local
-    function clearLibrary(){
-        if(localStorage.length === 0){
-            alert("There are no gratitudes to delete.");
-            window.location.reload();
-            return false;
+    }
+    
+    function doGroup(){
+        var doTag = document.getElementsByTagName("form");
+            selectLi = $('colors');
+            makeSelect = document.createElement('select');
+            makeSelect.setAttribute("id", "colorsGroups");
+        for(var i=0, j=colorsGroups.length; i<j; i++) {
+            var doOpt = document.createElement('option');
+            var doText = colorsGroups[i];
+            doOpt.setAttribute("value", doText);
+            doOpt.innerHTML = doText;
+            makeSelect.appendChild(doOpt);
         }
-    };
-        
-        //From local to browser
-        var makeConversion = document.createEl('div');
-        makeConversion.setAttribute("id", "choices");
-        var makeLi = document.createElement('ul');
-        makeConversion.appendChild(makeLi);
-        document.body.appendChild(makeConversion);
-        $('choices').style.display = "block";
-        for(var i=0, len=localStorage.length; i<len;i++) {
-            var makeLi = document.createElement('ol');
-            makeLi.appendChild(makeLi);
-            var key = localStorage.key(i);
-            var value = localStorage.getItem(key);
-            
-            //Local storage back to object
-            var obj = JSON.parse(value);
-            var makeAnotherLi = document.createElement('ul');
-            makeLi.appendChild(makeAnotherLi);
-            for(var n in obj) {
-                var makeSubli = document.createElement('ul');
-                makeAnotherLi.appendChild(makeSubli);
-                var optSubText = obj[n][0]+""+obj[n][1];
-                makeSubli.innerHTML = optSubText;
-            }
+        selectLi.appendChild(makeSelect);
+    }
+    
+    //Get Checkbox Value
+    function getCheckboxValue(){
+        if($('items').checked){
+            checkedValue = $('items').value;
         }
     }
     
-    //Display Grat data on other page
+        //Display Grat data on other page
     function toggleControls(n){
         switch(n){
             case "on":
-                $('addGratForm').style.display = "none";
+                $('gratitudeForm').style.display = "none";
                 $('clearGrat').style.display = "inline";
                 $('displayGrat').style.display = "none";
                 $('addNewGrat').style.display = "inline";
                 break;
             case "off":
-                $('addGratForm').style.display = "block";
+                $('gratitudeForm').style.display = "block";
                 $('clearGratForm').style.display = "inline";
                 $('displayGrat').style.display = "inline";
                 $('addNewGrat').style.display = "none";
@@ -67,52 +54,82 @@ window.addEventListener("DOMContentLoaded", function(){
             default:
                 return false;
         }
-    };
-    
-        //Get Checkbox Value
-    function getCheckboxValue(){
-        if($('items').checked){
-            checkedValue = $('items').value;
-        } else {
-            checkedValue = "No"
-           }
-        return checkedValue
-    };
-    
-        //Function to see local storage data
-    function showGrat (){
-        toggleControls("on");
-        if(localStorage.length === 0) {
-            alert("There are no Gratitudes in your library.");
-        }
-    };    
-    
+    }
+     
     //Store Grat in Local Storage
-    function storeGrat(){
-        var gratId = Math.floor(Math.random()*100000001)
-        
+    function storeData(){
+        var gratId = Math.floor(Math.random()*100000001);
+        getCheckboxValue();
         //Store form field values in an object
         //Objects props - array with form labels and input values
         var choice = {};
             choice.date = ["Date:", $('date').value];
             choice.time = ["Time:", $('time').value];
             
-            choice.what = ["What I'm grateful for:", $('what').value];
+            choice.what = ["What I'm grateful for:", $('gratitude').value];
             
-            choice.why = ["I have an attitude of gratitude because:", $('why').value];
+            choice.why = ["I have an attitude of gratitude because:", $('gratStory').value];
             
-            choice.color = ["What color do you like best today?:", getFavValue()];
+            choice.color = ["What color do you like best today?:",  $('colorsGroups').value];
             
-            choice.items = ["Which items make you happy?", itemsValue];
+            choice.items = ["Which items make you happy?", checkedValue];
     //Save data to local storage: conv obj to a string
-        localStorage.setChoice(gratId, JSON.stringify(choice));
+        localStorage.setItem(gratId, JSON.stringify(choice));
         alert("Gratitude Added!");
-    };
+    }
+        
+    //Function to see local storage data
+    function showGrat (){
+        toggleControls("on");
+        if(localStorage.length === 0) {
+            alert("There are no Gratitudes in your library.");
+        }
+        
+        //From local to browser
+        var makeConversion = document.createElement('div');
+        makeConversion.setAttribute("id", "choices");
+        var makeLi = document.createElement('ul');
+        makeConversion.appendChild(makeLi);
+        document.body.appendChild(makeConversion);
+        $('choices').style.display = "display";
+        for(var i=0, len=localStorage.length; i<len;i++) {
+            var doList = document.createElement('li');
+            var linksLi = document.createElement('li');
+            makeLi.appendChild(doList);
+            var key = localStorage.key(i);
+            var value = localStorage.getItem(key);
+            
+            //Local storage back to object
+            var obj = JSON.parse(value);
+            var makeAnotherLi = document.createElement('ul');
+            doList.appendChild(makeAnotherLi);
+            for(var n in obj) {
+                var makeSubli = document.createElement('li');
+                makeAnotherLi.appendChild(makeSubli);
+                var optSubText = obj[n][0] + " " + obj[n][1];
+                makeSubli.innerHTML = optSubText;
+            }
+        }
+    }    
+    
+    //Function - erase grats in local
+    function clearLibrary(){
+        if(localStorage.length === 0){
+            alert("There are no gratitudes to delete.");
+        }else{
+            localStorage.clear();
+            alert("All gratitudes have been deleted!");
+            window.location.reload();
+            return false;
+        }
+    }
     
     //Variable defaults
-    var colorsGroups = ["yellow", "pink", "blue", "green", "red"],
-        itemsValue
-        
+    var colorsGroups = ["--Colors--", "yellow", "pink", "blue", "green", "red"],
+        colors
+    ;
+    doGroup();
+       
     //Set link and submit click events
     var displayGrat = $("displayGrat");
     displayGrat.addEventListener("click", showGrat);
